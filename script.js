50084744e5e64d38c0884515cc28535dea47c023
Văn Tân <90215990+Vantan22@users.noreply.github.com>
2021-10-05 14:25:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ $(document).ready(function(){
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 tl.to(".text", { y: "0%", duration: 1, stagger: 0.5 });
-tl.to(".slider", { y: "-100%", duration: 2.47, delay: 0.5 });
+tl.to(".slider", { y: "-100%", duration: 2.5, delay: 0.5 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
