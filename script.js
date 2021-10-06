8ec434f37a1312e90a9bd07aebd96b1b73791987
Văn Tân <vantan407@gmail.com>
2021-10-06 23:30:35 +0700

    update

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        // sticky navbar on scroll script
-        // nếu kéo thanh menu trên 20 thì tạo class sticky
+        
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
-        }else{
+        }
+
+        else{
             $('.navbar').removeClass("sticky");
         }
-        // và ngược lại thì xóa nó đi
-        // scroll-up button show/hide script
+
+
+        //  tương tự như bên trên nhưng áp dụng với cái nút ấn để lên trang chủ
+     
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
             $('.scroll-up-btn').removeClass("show");
         }
+    
         if(this.scrollY > 500){
             $('.download-btn').addClass("show");
         }else{
@@ -22,25 +26,30 @@ $(document).ready(function(){
     });
 
     // slide-up script
+
+  
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
+ 
     });
 
     $('.navbar .menu li a').click(function(){
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
+  
     });
 
     // toggle menu/navbar script
-    // ấn vào menu tạo ra class active
+
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
     // typing text animation script
+
     var typed = new Typed(".typing", {
         strings: ["Van Tan", "Quoc Huy", "Dinh Hieu", "Nguyen Duong", "Phu Duc", "Viet Anh"],
         typeSpeed: 100,
@@ -87,6 +96,7 @@ $(document).ready(function(){
 
     // owl carousel script
     $('.carousel').owlCarousel({
+
         margin: 20,
         loop: true,
         autoplay: true,
